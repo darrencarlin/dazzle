366bbbfb6b8daebeb8bda16c30937f256ea41ebd
Darren Carlin <daz@Darrens-MBP.home>
2018-08-02 19:41:15 -0400

    tool bar revert

--- src/js/script.js
+++ src/js/script.js
@@ -20,9 +20,9 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
   const elements = Array.from(document.getElementsByTagName("*"));
   let elements2 = Array.from(document.getElementsByTagName("*"));
-  elements2.splice(0, 12);
-  elements2.splice(1, 10);
-  elements2.splice(33, 1);
+  elements2.splice(0, 1);
+  elements2.splice(1, 8);
+  elements2.splice(38, 1);
   console.log(elements2);
   const toolbar = document.getElementById("toolbar");
   const cog = document.getElementById("cog");
